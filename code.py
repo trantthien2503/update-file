102160b90b0978e074086516d2f95eac6f154bed
trantthien2503 <49374309+trantthien2503@users.noreply.github.com>
2025-01-16 14:35:04 +0700

    Update code.py

--- code.py
+++ code.py
@@ -1,74 +1,50 @@
-from pox.core import core
-import pox.openflow.libopenflow_01 as of
+#!/usr/bin/python
 
-log = core.getLogger()
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+from mininet.cli import CLI
+from mininet.node import RemoteController
 
-class Part3Controller(object):
-    def __init__(self, connection):
-        self.connection = connection
-        self.dpid = connection.dpid  # Datapath ID for the switch
-        connection.addListeners(self)
-        self.install_rules()
+class Part4Topo(Topo):
+    def build(self):
+        # Add switches
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        cores21 = self.addSwitch('cores21')
+        dcs31 = self.addSwitch('dcs31')
 
-    def install_rules(self):
-        # Add flooding rules for secondary switches
-        if self.dpid in [1, 2, 3, 4]:  # Secondary switches: s1, s2, s3, dcs31
-            self.add_flood_rule()
-        elif self.dpid == 21:  # Core switch: cores21
-            self.add_core_rules()
+        # Add hosts
+        h10 = self.addHost('h10', mac='00:00:00:00:00:01', ip='10.0.1.10/24', defaultRoute='via 10.0.1.1')
+        h20 = self.addHost('h20', mac='00:00:00:00:00:02', ip='10.0.2.20/24', defaultRoute='via 10.0.2.1')
+        h30 = self.addHost('h30', mac='00:00:00:00:00:03', ip='10.0.3.30/24', defaultRoute='via 10.0.3.1')
+        serv1 = self.addHost('serv1', mac='00:00:00:00:00:04', ip='10.0.4.10/24', defaultRoute='via 10.0.4.1')
+        hnotrust1 = self.addHost('hnotrust1', mac='00:00:00:00:00:05', ip='172.16.10.100/24', defaultRoute='via 172.16.10.1')
 
-    def add_flood_rule(self):
-        # Flood traffic on secondary switches
-        msg = of.ofp_flow_mod()
-        msg.priority = 10
-        msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
-        self.connection.send(msg)
-        log.info(f"Flood rule installed on switch {self.dpid}")
+        # Add links
+        self.addLink(h10, s1)
+        self.addLink(h20, s2)
+        self.addLink(h30, s3)
+        self.addLink(s1, cores21)
+        self.addLink(s2, cores21)
+        self.addLink(s3, cores21)
+        self.addLink(serv1, dcs31)
+        self.addLink(cores21, dcs31)
+        self.addLink(hnotrust1, cores21)
 
-    def add_core_rules(self):
-        # Core switch: Route based on destination IP
-        # Route traffic to h10 (subnet 10.0.1.0/24)
-        self.add_routing_rule("10.0.1.0/24", 1)
-        # Route traffic to h20 (subnet 10.0.2.0/24)
-        self.add_routing_rule("10.0.2.0/24", 2)
-        # Route traffic to h30 (subnet 10.0.3.0/24)
-        self.add_routing_rule("10.0.3.0/24", 3)
-        # Route traffic to serv1 (subnet 10.0.4.0/24)
-        self.add_routing_rule("10.0.4.0/24", 4)
+topos = {'part4': Part4Topo}
 
-        # Block all IP traffic from hnotrust1 to serv1
-        self.add_block_rule("172.16.10.100", "10.0.4.10", 0x0800)
+def configure():
+    topo = Part4Topo()
+    net = Mininet(topo=topo, controller=RemoteController)
+    net.start()
 
-        # Block ICMP traffic from hnotrust1 to internal hosts
-        self.add_block_rule("172.16.10.100", "10.0.0.0/8", 0x0800, nw_proto=1)
+    CLI(net)
 
-    def add_routing_rule(self, nw_dst, port):
-        msg = of.ofp_flow_mod()
-        msg.priority = 20
-        msg.match.dl_type = 0x0800  # IP traffic
-        msg.match.nw_dst = nw_dst
-        msg.actions.append(of.ofp_action_output(port=port))
-        self.connection.send(msg)
-        log.info(f"Routing rule for {nw_dst} -> port {port} added on switch {self.dpid}")
+    net.stop()
 
-    def add_block_rule(self, nw_src, nw_dst, dl_type, nw_proto=None):
-        msg = of.ofp_flow_mod()
-        msg.priority = 30
-        msg.match.dl_type = dl_type  # Match on IP traffic
-        msg.match.nw_src = nw_src
-        msg.match.nw_dst = nw_dst
-        if nw_proto is not None:
-            msg.match.nw_proto = nw_proto  # Optional: Match on ICMP
-        self.connection.send(msg)
-        log.info(f"Block rule: {nw_src} -> {nw_dst} added on switch {self.dpid}")
 
-    def _handle_PacketIn(self, event):
-        # Log unhandled packets
-        log.info(f"Unhandled packet from switch {self.dpid}: {event.parsed}")
-
-def launch():
-    def start_switch(event):
-        log.info(f"Switch {event.connection.dpid} connected")
-        Part3Controller(event.connection)
-
-    core.openflow.addListenerByName("ConnectionUp", start_switch)
+if __name__ == '__main__':
+    configure()
