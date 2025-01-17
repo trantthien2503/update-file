dd4a6647b845a3a088ac34c46166b3f21cd89ed1
trantthien2503 <49374309+trantthien2503@users.noreply.github.com>
2025-01-17 09:45:25 +0700

    Update part3controller

--- code.py
+++ code.py
@@ -1,149 +1,131 @@
+# Part 3 0f Project 2
+
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
-from pox.lib.packet.ethernet import ethernet
-from pox.lib.packet.arp import arp
-from pox.lib.packet.ipv4 import ipv4
-from pox.lib.addresses import EthAddr, IPAddr
+from pox.lib.addresses import IPAddr, IPAddr6, EthAddr
+import pox.lib.packet as pkt
 
 log = core.getLogger()
 
+#statically allocate a routing table for hosts
+#MACs used in only in part 4
+IPS = {
+  "h10" : ("10.0.1.10", '00:00:00:00:00:01'),
+  "h20" : ("10.0.2.20", '00:00:00:00:00:02'),
+  "h30" : ("10.0.3.30", '00:00:00:00:00:03'),
+  "serv1" : ("10.0.4.10", '00:00:00:00:00:04'),
+  "hnotrust" : ("172.16.10.100", '00:00:00:00:00:05'),
+}
 
-class Part4Controller(object):
-    def __init__(self, connection):
-        self.connection = connection
-        self.dpid = connection.dpid
-        self.arp_table = {}  # L3 -> L2 mappings (IP -> (port, MAC))
+class Part3Controller (object):
+  """
+  A Connection object for that switch is passed to the __init__ function.
+  """
+  def __init__ (self, connection):
+    print (connection.dpid)
+    # Keep track of the connection to the switch so that we can
+    # send it messages!
+    self.connection = connection
 
-        # Pre-populate ARP table with gateway mappings
-        self.gateway_mac = EthAddr("00:00:00:00:00:01")
-        self.gateways = {
-            "10.0.1.1": 1,
-            "10.0.2.1": 2,
-            "10.0.3.1": 3,
-            "10.0.4.1": 4,
-            "172.16.10.1": 5
-        }
-        for ip, port in self.gateways.items():
-            self.arp_table[IPAddr(ip)] = (port, self.gateway_mac)
+    # This binds our PacketIn event listener
+    connection.addListeners(self)
+    #use the dpid to figure out what switch is being created
+    if (connection.dpid == 1):
+      self.s1_setup()
+    elif (connection.dpid == 2):
+      self.s2_setup()
+    elif (connection.dpid == 3):
+      self.s3_setup()
+    elif (connection.dpid == 21):
+      self.cores21_setup()
+    elif (connection.dpid == 31):
+      self.dcs31_setup()
+    else:
+      print ("UNKNOWN SWITCH")
+      exit(1)
 
-        connection.addListeners(self)
+  def s1_setup(self):
+    self._allow_all()
 
-        # Install default rules
-        if self.dpid in [1, 2, 3, 4]:  # Secondary switches
-            self.install_flood_rule()
-        elif self.dpid == 21:  # Core switch (cores21)
-            self.install_block_rules()
+  def s2_setup(self):
+    self._allow_all()
 
-    def install_flood_rule(self):
-        # Flood traffic on secondary switches
-        msg = of.ofp_flow_mod()
-        msg.priority = 10
-        msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
-        self.connection.send(msg)
-        log.info(f"Flood rule installed on switch {self.dpid}")
+  def s3_setup(self):
+    self._allow_all()
 
-    def install_block_rules(self):
-        # Block IP traffic from hnotrust1 to serv1
-        self.add_block_rule("172.16.10.100", "10.0.4.10", 0x0800)
+  def cores21_setup(self):
+    self._block()                               # block comm.s w/hnotrust
+    self._internal_to_external()                # guide traffic through sw
+    self._allow_all()                           # flood/drop the rest
 
-        # Block ICMP traffic from hnotrust1 to internal hosts and serv1
-        self.add_block_rule("172.16.10.100", "10.0.0.0/8", 0x0800, nw_proto=1)
+  def dcs31_setup(self):
+    self._allow_all()
 
-    def add_block_rule(self, nw_src, nw_dst, dl_type, nw_proto=None):
-        msg = of.ofp_flow_mod()
-        msg.priority = 30
-        msg.match.dl_type = dl_type
-        msg.match.nw_src = nw_src
-        msg.match.nw_dst = nw_dst
-        if nw_proto is not None:
-            msg.match.nw_proto = nw_proto
-        self.connection.send(msg)
-        log.info(f"Block rule: {nw_src} -> {nw_dst} installed on cores21")
+  # flood all communications going to through the net, dropping the rest
+  def _allow_all(self, act=of.ofp_action_output(port=of.OFPP_FLOOD)):
+    self.connection.send(of.ofp_flow_mod(action=act,
+                                         priority=2))     # flood to all ports
+    # otherwise, iperfs will hang
+    self.connection.send(of.ofp_flow_mod(priority=1))
+  
+  # block ICMP from hnotrust to anyone, and block all IP to serv1
+  def _block(self, block=IPS['hnotrust'][0]):
+    block_icmp = of.ofp_flow_mod(priority=20,
+                                 match=of.ofp_match(dl_type=0x800,
+                                                    nw_proto=pkt.ipv4.ICMP_PROTOCOL,
+                                                    nw_src=block))
+    self.connection.send(block_icmp)
+    block_to_serv = of.ofp_flow_mod(priority=19,
+                                 match=of.ofp_match(dl_type=0x800,
+                                                    nw_src=block,
+                                                    nw_dst=IPS['serv1'][0]))
+    self.connection.send(block_to_serv)
+  
+  # allow IP traffic as normal
+  def _internal_to_external(self):
+    host = {10: (IPS['h10'][0], 1),
+            20: (IPS['h20'][0], 2),
+            30: (IPS['h30'][0], 3),
+            40: (IPS['serv1'][0], 4),
+            50: (IPS['hnotrust'][0], 5)}
+    
+    for i in range(len(host)):
+      h = host[(i+1)*10][0]
+      p = host[(i+1)*10][1]
+      self.connection.send(of.ofp_flow_mod(action=of.ofp_action_output(port=p),
+                                           priority=5,
+                                           match=of.ofp_match(dl_type=0x800,
+                                                              nw_dst=h)))
 
-    def _handle_PacketIn(self, event):
-        packet = event.parsed
+  #used in part 4 to handle individual ARP packets
+  #not needed for part 3 (USE RULES!)
+  #causes the switch to output packet_in on out_port
+  def resend_packet(self, packet_in, out_port):
+    msg = of.ofp_packet_out()
+    msg.data = packet_in
+    action = of.ofp_action_output(port = out_port)
+    msg.actions.append(action)
+    self.connection.send(msg)
 
-        # Handle ARP packets
-        if packet.type == ethernet.ARP_TYPE:
-            self.handle_arp(packet, event)
-            return
+  def _handle_PacketIn (self, event):
+    """
+    Packets not handled by the router rules will be
+    forwarded to this method to be handled by the controller
+    """
 
-        # Handle IPv4 packets
-        if packet.type == ethernet.IP_TYPE:
-            self.handle_ip(packet, event)
-            return
+    packet = event.parsed # This is the parsed packet data.
+    if not packet.parsed:
+      log.warning("Ignoring incomplete packet")
+      return
 
-        # Log unsupported packet types
-        log.info(f"Unhandled packet type {packet.type}, dropping packet")
+    packet_in = event.ofp # The actual ofp_packet_in message.
+    print('Unhandled packet from '+str(self.connection.dpid)+':'+packet.dump())
 
-    def handle_arp(self, packet, event):
-        arp_pkt = packet.payload
-
-        # Learn ARP mappings
-        self.arp_table[arp_pkt.protosrc] = (event.port, packet.src)
-        log.info(f"Learned ARP mapping: {arp_pkt.protosrc} -> {packet.src} on port {event.port}")
-
-        # Respond to ARP requests for the gateway
-        if arp_pkt.opcode == arp.REQUEST and arp_pkt.protodst in self.gateways:
-            self.send_arp_reply(arp_pkt, event.port)
-
-    def send_arp_reply(self, arp_pkt, port):
-        # Create ARP reply
-        arp_reply = arp()
-        arp_reply.opcode = arp.REPLY
-        arp_reply.hwdst = arp_pkt.hwsrc
-        arp_reply.protodst = arp_pkt.protosrc
-        arp_reply.hwsrc = self.gateway_mac
-        arp_reply.protosrc = arp_pkt.protodst
-
-        # Create Ethernet frame
-        eth = ethernet()
-        eth.type = ethernet.ARP_TYPE
-        eth.src = self.gateway_mac
-        eth.dst = arp_pkt.hwsrc
-        eth.payload = arp_reply
-
-        # Send ARP reply
-        msg = of.ofp_packet_out()
-        msg.data = eth.pack()
-        msg.actions.append(of.ofp_action_output(port=port))
-        self.connection.send(msg)
-        log.info(f"Sent ARP reply for {arp_pkt.protodst} to {arp_pkt.protosrc}")
-
-    def handle_ip(self, packet, event):
-        ip_pkt = packet.payload
-
-        # Learn source IP mapping
-        self.arp_table[ip_pkt.srcip] = (event.port, packet.src)
-        log.info(f"Learned IP mapping: {ip_pkt.srcip} -> {packet.src} on port {event.port}")
-
-        # Forward packet if destination IP is known
-        if ip_pkt.dstip in self.arp_table:
-            port, mac = self.arp_table[ip_pkt.dstip]
-            self.install_forwarding_rule(ip_pkt.dstip, mac, port)
-            self.forward_packet(packet, port)
-        else:
-            log.info(f"Unknown destination {ip_pkt.dstip}, dropping packet")
-
-    def install_forwarding_rule(self, nw_dst, dst_mac, port):
-        msg = of.ofp_flow_mod()
-        msg.priority = 20
-        msg.match.dl_type = 0x0800
-        msg.match.nw_dst = nw_dst
-        msg.actions.append(of.ofp_action_dl_addr.set_dst(dst_mac))
-        msg.actions.append(of.ofp_action_output(port=port))
-        self.connection.send(msg)
-        log.info(f"Installed forwarding rule for {nw_dst} -> port {port}")
-
-    def forward_packet(self, packet, port):
-        msg = of.ofp_packet_out()
-        msg.data = packet.pack()
-        msg.actions.append(of.ofp_action_output(port=port))
-        self.connection.send(msg)
-
-def launch():
-    def start_switch(event):
-        log.info(f"Switch {event.connection.dpid} connected")
-        Part4Controller(event.connection)
-
-    core.openflow.addListenerByName("ConnectionUp", start_switch)
+def launch ():
+  """
+  Starts the component
+  """
+  def start_switch (event):
+    log.debug("Controlling %s" % (event.connection,))
+    Part3Controller(event.connection)
+  core.openflow.addListenerByName("ConnectionUp", start_switch)
